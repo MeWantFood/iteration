63bd95f4342ec6076c8e7085e44c430d7fe7d7e8
KyEBell <kyleevanbell@gmail.com>
2023-07-05 12:32:38 -0400

    pushing up currently not working test file for others to pull and look through

--- __test__/react.test.js
+++ __test__/react.test.js
@@ -1,21 +1,22 @@
-const path = require('path');
+// const path = require('path');
 // const app = require(path.resolve(__dirname, './server/server.js'));
+import React from 'react';
+import { render } from '@testing-library/react';
+import Signup from '../client/components/Signup';
+import Login from '../client/components/Login';
+// import App from '../client/components/App';
+
 // const Signup = require('./../client/components/Signup.jsx');
 // import { Provider } from 'react-redux';
 // import userEvent from '@testing-library/user-event';
 // import regeneratorRuntime from 'regenerator-runtime';
-// import App from '../client/components/App';
 // import App from '../client/components/Card';
-
-import React from 'react';
-// import { render } from '@testing-library/react';
-import Signup from '../client/components/Signup';
-import Login from '../client/components/Login';
 
 describe('testing react stuffs', () => {
   describe('Signup', () => {
     let text;
     let otherText;
+
     beforeAll(() => {
       text = render(<Signup />);
       otherText = render(<Login />);
